387c10b8b4c4a6980a9746532947cc5d6e0d0f5b
jacek <kalbarczyk@gmail.com>
2018-05-19 21:22:46 +0200

    Hackathon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.static_path = path.join(path.abspath(__file__), 'static')
+# app.static_path = path.join(path.abspath(__file__), 'static')
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -23,23 +23,27 @@ Bootstrap(app)
 
 db.init_app(app)
 
+# with app.test_request_context():
+#     from auth.models import User
+#     from invoices.models import Products, Customers, Invoices, Basket, Quantities
+#     db.create_all()
+
 from home.views import home_blueprint
 from auth.views import auth_blueprint
+from invoices.views import invoices_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(auth_blueprint)
+app.register_blueprint(invoices_blueprint)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 
-engine = create_engine('postgresql://postgres:postgres@localhost/stock_manager', convert_unicode=True, echo=False)
-Session = sessionmaker(bind=engine)
+# with app.app_context():
+#     db.init_app(app)
 
-# if(db.engine):
-#     print("Tak")
-# else:
-#     print("nie")
+
 from auth.models import User
 @login_manager.user_loader
 def load_user(user_id):
