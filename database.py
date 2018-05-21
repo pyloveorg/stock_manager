1533edc37521c163aefb85454e6dea89ab96c57b
jacek <kalbarczyk@gmail.com>
2018-05-21 21:43:00 +0200

    CommitItIs

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
 db = SQLAlchemy()
-def create_app():
-    app = Flask(__name__)
-    db.init_app()
-    db.app = app
-    db.create_all()
-    return app
+# def create_app():
+#     app = Flask(__name__)
+#     db.init_app()
+#     db.app = app
+#     db.create_all()
+#     return app
